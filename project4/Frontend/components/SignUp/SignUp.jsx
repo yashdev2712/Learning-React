7c01bc262afcd35319607692249542e9b0550d90
yashdev2712 <deveshchandra2712@gmail.com>
2024-05-29 04:55:00 +0530

    latest

--- project4/Frontend/components/SignUp/SignUp.jsx
+++ project4/Frontend/components/SignUp/SignUp.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import "./SignUp.css"
+import axios from 'axios';
 
 function SignUp() {
+    const [userName, setUserName] = useState(null);
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null);
+    const [trigger, setTrigger] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (trigger) {
+            const sendDataToBackend = async () => {
+                try {
+                    const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+                        userName: userName,
+                        email: email,
+                        password: password
+                    })
+
+                    if (response.status = 200) {
+                        navigate("/message")
+                    }
+                }
+                catch (err) {
+                    console.log("error:", err);
+                }
+                finally {
+                    setTrigger(false)
+                }
+            }
+            sendDataToBackend();
+        }
+    }, [userName, email, password, trigger, navigate])
+
+    const handleOnClick = () => {
+        setTrigger(true);
+    }
     return (
         <>
             <div className='main'>
                 <div className='content'>
                     <h1>Sign Up</h1>
                     <div className="input">
-                        <input type="text" placeholder='Enter your first name' />
-                        <input type="text" placeholder='Enter your last name' />
-                        <input type="text" placeholder='Enter your username' />
-                        <input type="password" placeholder='Enter your passwords' />
+                        <input type="text" placeholder='Enter your User Name' onChange={(e) => { setUserName(e.target.value) }} />
+                        <input type="text" placeholder='Enter your Email' onChange={(e) => { setEmail(e.target.value) }} />
+                        <input type="password" placeholder='Enter your Password' onChange={(e) => { setPassword(e.target.value) }} />
                     </div>
-                    <button type='submit'>Login</button>
+                    <button type='submit' onClick={() => { handleOnClick() }}>Login</button>
                     <span><Link to="/login">Already a User</Link> </span>
                 </div>
             </div>
